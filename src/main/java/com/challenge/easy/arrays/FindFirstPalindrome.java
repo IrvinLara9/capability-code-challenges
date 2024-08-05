a39ef9854bdc15905b5bd9f6d0e5a49d75b200a3
Irvin Lara <irvinlaravillalobos@yahoo.com.mx>
2024-08-05 15:34:53 -0600

    Add FindFirstPalindrome solution

--- src/main/java/com/challenge/easy/arrays/FindFirstPalindrome.java
+++ src/main/java/com/challenge/easy/arrays/FindFirstPalindrome.java
@@ -1,36 +1,53 @@
 package com.challenge.easy.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
-     Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
-
-     A string is palindromic if it reads the same forward and backward.
-
-     Example 1:
-         Input: words = ["abc","car","ada","racecar","cool"]
-         Output: "ada"
-         Explanation: The first string that is palindromic is "ada".
-         Note that "racecar" is also palindromic, but it is not the first.
-
-     Example 2:
-         Input: words = ["notapalindrome","racecar"]
-         Output: "racecar"
-         Explanation: The first and only string that is palindromic is "racecar".
-
-     Example 3:
-         Input: words = ["def","ghi"]
-         Output: ""
-         Explanation: There are no palindromic strings, so the empty string is returned.
-
-     Constraints:
-
-         * 1 <= words.length <= 100
-         * 1 <= words[i].length <= 100
-         * words[i] consists only of lowercase English letters.
+ * Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+ * <p>
+ * A string is palindromic if it reads the same forward and backward.
+ * <p>
+ * Example 1:
+ * Input: words = ["abc","car","ada","racecar","cool"]
+ * Output: "ada"
+ * Explanation: The first string that is palindromic is "ada".
+ * Note that "racecar" is also palindromic, but it is not the first.
+ * <p>
+ * Example 2:
+ * Input: words = ["notapalindrome","racecar"]
+ * Output: "racecar"
+ * Explanation: The first and only string that is palindromic is "racecar".
+ * <p>
+ * Example 3:
+ * Input: words = ["def","ghi"]
+ * Output: ""
+ * Explanation: There are no palindromic strings, so the empty string is returned.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 100
+ * words[i] consists only of lowercase English letters.
  */
 public class FindFirstPalindrome {
 
     public static String firstPalindrome(String[] words) {
-        return null;
+        for (String currentWord : words) {
+            int wordLength = currentWord.length();
+            boolean isPalindrome = true;
+
+            for (int j = 0; j < (wordLength / 2); j++) {
+                char firstLetter = currentWord.charAt(j);
+                char lastLetter = currentWord.charAt(wordLength - j - 1);
+                if (firstLetter != lastLetter) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome) return currentWord;
+        }
+        return "";
     }
 
     public static void main(String[] args) {
