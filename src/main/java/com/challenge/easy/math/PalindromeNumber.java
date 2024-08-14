c5263b15ab91f42756a81534407c4790d751ce28
Irvin Lara <irvinlaravillalobos@yahoo.com.mx>
2024-08-14 11:25:38 -0600

    Add PalindromeNumber algorithm solution

--- src/main/java/com/challenge/easy/math/PalindromeNumber.java
+++ src/main/java/com/challenge/easy/math/PalindromeNumber.java
@@ -30,7 +30,21 @@ package com.challenge.easy.math;
 public class PalindromeNumber {
 
     public static boolean isPalindrome(int x) {
-        return false;
+        int original = x;
+        int reversed = 0;
+
+        if (x < 0) {
+            return false;
+        }
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+
+            x = x / 10;
+        }
+
+        return original == reversed;
     }
 
     public static void main(String[] args) {
